e93cbce6add618435ea81d81958e585d2c8b7c59
spongeyperson1000@live.com <spongeyperson1000@live.com>
2021-11-17 18:08:22 -0800

    Updated Configs

--- home/tyler/.config/fish/config.fish
+++ home/tyler/.config/fish/config.fish
@@ -15,11 +15,12 @@ alias ...="cd -"
 #Manipulate KDE
 alias lockwidgets="qdbus org.kde.plasmashell /PlasmaShell evaluateScript 'lockCorona(true)'"
 alias unlockwidgets="qdbus org.kde.plasmashell /PlasmaShell evaluateScript 'lockCorona(false)'"
-alias restartkwin="killall kwin_x11 ; kstart5 kwin_x11 --replace &"
-alias restartplasma="killall plasmashell ; kstart5 plasmashell &"
+alias restartkwin="killall kwin_x11 ; nohup kstart5 kwin_x11 </dev/null &>/dev/null & ; echo -e '\033[0;32mKwin Restarted'"
+alias restartplasma="killall plasmashell ; nohup kstart5 plasmashell </dev/null &>/dev/null & ; echo -e '\033[0;32mPlasma Shell Restarted'"
+alias restartdock="killall latte-dock ; nohup kstart5 latte-dock </dev/null &>/dev/null & ; echo -e '\033[0;32mLatte Dock Restarted'"
 
 #Dotfiles Sync
-alias config="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=/'
 
 #Radeon Vulkan Settings
 alias radv="VK_ICD_FILENAMES='/usr/share/vulkan/icd.d/radeon_icd.x86_64.json'"
